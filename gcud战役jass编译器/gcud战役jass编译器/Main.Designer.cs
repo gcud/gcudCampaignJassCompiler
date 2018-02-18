c1cb8ab0b49ed9d5a94de92fddc603f31fee82b1
gcud <gcudwork@163.com>
2018-02-18 15:23:56 +0800

    git同步

---
+++ gcud战役jass编译器/gcud战役jass编译器/Main.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace gcud战役jass编译器
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "魔兽根路径";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(83, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(624, 21);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "D:\\Game\\War3\\War3_1.27CN";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(276, 82);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "复制压缩的bj到桌面";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(258, 123);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(164, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "生成编译后的gcud混乱之治jass";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(252, 213);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(164, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "生成编译后的bj";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(695, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "此功能依赖于gcud预先制作的blizzard_base.txt，该文件位于与程序同级的jass目录下面，如果丢失会导致整个程序无法进行操作";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 149);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(569, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "此战役目录新建jass文件夹,并新建Variable,Function,Filter,Skill,Trigger,Init6个txt，在里面写jass";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(83, 33);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(624, 21);
+            this.textBox2.TabIndex = 8;
+            this.textBox2.Text = "ReignOfChaos";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 36);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 12);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "战役目录";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(81, 67);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(341, 12);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "要编译的文件必须在魔兽战役文件夹新建目录，只能为一级目录";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(83, 164);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(606, 21);
+            this.textBox3.TabIndex = 11;
+            this.textBox3.Text = "InitReignofChaos_gcud";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 167);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 12);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "入口函数名";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 190);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(53, 12);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "编译选项";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(72, 190);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(96, 16);
+            this.checkBox1.TabIndex = 13;
+            this.checkBox1.Text = "移除空白字符";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(174, 191);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(72, 16);
+            this.checkBox2.TabIndex = 14;
+            this.checkBox2.Text = "移除空行";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Checked = true;
+            this.checkBox3.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox3.Location = new System.Drawing.Point(252, 191);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(72, 16);
+            this.checkBox3.TabIndex = 15;
+            this.checkBox3.Text = "移除注释";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Checked = true;
+            this.checkBox4.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox4.Enabled = false;
+            this.checkBox4.Location = new System.Drawing.Point(330, 191);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(132, 16);
+            this.checkBox4.TabIndex = 16;
+            this.checkBox4.Text = "简化名称（还没做）";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(735, 446);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "gcud战役jass编译器";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+    }
+}
+
---
+++ gcud战役jass编译器/gcud战役jass编译器/Main.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace gcud战役jass编译器
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+        public string CampaignPath
+        {
+            get
+            { return textBox1.Text + "/Campaigns/" + textBox2.Text + "/"; }
+        }
+        public string CurrentPath
+        {
+            get
+            {
+                return Directory.GetCurrentDirectory();
+            }
+        }
+        public string DesktopPath
+        {
+            get
+            {
+                return Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            }
+        }
+        public string BJPath
+        {
+            get
+            {
+                return CurrentPath + "/" + "jass/blizzard_base.txt";
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string FilePath = DesktopPath + "/" + Path.GetFileName(BJPath);
+            File.Copy(BJPath, FilePath, true);
+            MessageBox.Show("blizzrd_base.txt已经复制到桌面");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("还没做");
+            //string DesktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            //string jassPath = CurrentPath + "/" + "jass/blizzard_base.j";
+            //List<string> a = new List<string>();
+            //string[] b = File.ReadAllLines(jassPath);
+            //foreach (string c in b)
+            //{
+            //    if (c[0] != '/')
+            //    {
+            //        a.Add(c);
+            //    }
+            //}
+            //File.WriteAllLines(DesktopPath + "/a.txt", a); ;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //移除空白字符
+            bool RemoveBlankCharacter = checkBox1.Checked;
+            //移除空行
+            bool RemoveBlankLine = checkBox2.Checked;
+            //移除注释
+            bool RemoveComment = checkBox3.Checked;
+            //简化名称,暂时不做
+            bool SimplifyName = checkBox4.Checked;
+            List<string> Variable = new List<string>(), Function = new List<string>(), Filter = new List<string>(), Skill = new List<string>(), Trigger = new List<string>(), Init = new List<string>();
+            string[] jassList = { "Variable", "Function", "Filter", "Skill", "Trigger", "Init" };
+            foreach (string item in jassList)
+            {
+                StreamReader s = new StreamReader(CampaignPath + "/jass/" + item + ".txt");
+                string line;
+                while ((line = s.ReadLine()) != null)
+                {
+
+                    if (RemoveBlankCharacter)
+                    {
+                        line = line.Trim();
+                    }
+                    if (RemoveBlankLine && line == "")
+                    {
+                        continue;
+                    }
+                    if (RemoveComment && line.TrimStart()[0] == '/')
+                    {
+                        continue;
+                    }
+                    switch (item)
+                    {
+                        case "Variable":
+                            Variable.Add(line);
+                            break;
+                        case "Function":
+                            Function.Add(line);
+                            break;
+                        case "Filter":
+                            Filter.Add(line);
+                            break;
+                        case "Skill":
+                            Skill.Add(line);
+                            break;
+                        case "Trigger":
+                            Trigger.Add(line);
+                            break;
+                        case "Init":
+                            Init.Add(line);
+                            break;
+                    }
+                }
+                s.Close();
+            }
+            string bjAddInit = "call "+textBox3.Text+"()";
+            List<string> bj = new List<string>(File.ReadAllLines(BJPath));
+            bj.InsertRange(1, Variable);
+            bj.InsertRange(6342+Variable.Count, Function);
+            bj.InsertRange(6342 + Variable.Count+Function.Count, Filter);
+            bj.InsertRange(6342 + Variable.Count + Function.Count + Filter.Count, Skill);
+            bj.InsertRange(6342 + Variable.Count + Function.Count + Filter.Count+ Skill.Count, Trigger);
+            bj.InsertRange(6342 + Variable.Count + Function.Count + Filter.Count + Skill.Count + Trigger.Count, Init);
+            bj.Insert(6352 + Variable.Count + Function.Count + Filter.Count + Skill.Count + Trigger.Count+Init.Count, bjAddInit);
+            File.WriteAllLines(DesktopPath + "/blizzard.j", bj);
+            MessageBox.Show("已编译blizzard.j并保存到桌面");
+        }
+    }
+}
